d1a3e8ae3f3f4e1dcddaaaff65ee07b50bc0fb6a
Zac Sturgess <zac@sturgessweb.com>
2013-03-05 19:15:36 +0000

    Added more help

--- src/me/zsturgess/banlogger/LoggerMethods.java
+++ src/me/zsturgess/banlogger/LoggerMethods.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandSender;
 
 public class LoggerMethods {
     public static BanLogger plugin;
-    public static LoggerMethods lib;
     
     //Utility Methods
     public static boolean isInt(String str){
@@ -22,9 +21,9 @@ public class LoggerMethods {
         player.sendMessage(ChatColor.GOLD + "[BanLogger]" + ChatColor.RESET + " Showing help page " + page + "/y:");
         if(page == 1){
             player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see" + ChatColor.RESET + ", Show the last " + plugin.getConfig().getString("cache-days") + " days of logs.");
+            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see" + ChatColor.RESET + ", Show the last few days of logs.");
             player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see last #" + ChatColor.RESET + ", Show the last # days of logs.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see <user>" + ChatColor.RESET + ", Show the last " + plugin.getConfig().getString("cache-days") + " days of user's logs.");
+            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see <user>" + ChatColor.RESET + ", Show the last few days of user's logs.");
             player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see <user> #" + ChatColor.RESET + ", Show the last # days of user's logs.");
             player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log post <msg>" + ChatColor.RESET + ", Post a message to all other administrators.");
             player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log read" + ChatColor.RESET + ", Show all active messages. (Messages are deleted after 30 days)");
