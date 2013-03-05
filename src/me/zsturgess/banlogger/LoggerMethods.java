2656b93078e3e9b102ec2573b562f3978b222a96
Zac Sturgess <zac@sturgessweb.com>
2013-03-05 19:24:02 +0000

    Added more help

--- src/me/zsturgess/banlogger/LoggerMethods.java
+++ src/me/zsturgess/banlogger/LoggerMethods.java
@@ -20,16 +20,18 @@ public class LoggerMethods {
     public static boolean showHelp(CommandSender player, Integer page){
         player.sendMessage(ChatColor.GOLD + "[BanLogger]" + ChatColor.RESET + " Showing help page " + page + "/y:");
         if(page == 1){
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see" + ChatColor.RESET + ", Show the last few days of logs.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see last #" + ChatColor.RESET + ", Show the last # days of logs.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see <user>" + ChatColor.RESET + ", Show the last few days of user's logs.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log see <user> #" + ChatColor.RESET + ", Show the last # days of user's logs.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log post <msg>" + ChatColor.RESET + ", Post a message to all other administrators.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log read" + ChatColor.RESET + ", Show all active messages. (Messages are deleted after 30 days)");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
-            player.sendMessage(ChatColor.GOLD + ">>" + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log #" + ChatColor.RESET + ", Show the #'th page of help.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log see" + ChatColor.RESET + ", Show the last few days of logs.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log see last #" + ChatColor.RESET + ", Show the last # days of logs.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log see <user>" + ChatColor.RESET + ", Show the last few days of user's logs.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log see <user> #" + ChatColor.RESET + ", Show the last # days of user's logs.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log remove #" + ChatColor.RESET + ", Remove log entry #. (Does not undo the action)");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log post <msg>" + ChatColor.RESET + ", Post a message to all other administrators.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log read" + ChatColor.RESET + ", Show all active messages. (Messages are deleted after 30 days)");
+        }else if(page==2){
+            String stuff = "";
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RED + "/log <action> <user> <reason>" + ChatColor.RESET + ", Log & Do action for reason to user.");
+            player.sendMessage(ChatColor.GOLD + ">> " + ChatColor.RESET + "Possible Actions: " + stuff);
         }
         return true;
     }
